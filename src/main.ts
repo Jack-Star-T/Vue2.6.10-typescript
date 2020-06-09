a647178258e6381fed7e5bb3dea923c80d838a20
Jack-Star-T <59675168+Jack-Star-T@users.noreply.github.com>
2020-06-09 09:08:35 +0800

    Update main.ts
    axios拦截器引入

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element'
+import './interceptors/index'
 
 Vue.config.productionTip = false
 
