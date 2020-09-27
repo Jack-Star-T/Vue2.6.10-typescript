b4e8b3cbeac3397354fd28db70597c8a33bd7d94
凉梦 <1206986819@qq.com>
2020-09-27 12:50:50 +0800

    新增相对时间，例如1天前，2小时前，十分钟前，方法封装在filters文件夹下，使用插件moment.js

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 
+// @ts-ignore
+import moment from "moment";
+
+moment.locale('zh-cn');
+
 //例如 18045486451 显示为180****6451
 Vue.filter('formatMobile', (value:string)=>{
   let re=/(\d{3})\d{4}(\d{4})/;
@@ -19,4 +24,54 @@ Vue.filter('date', (value:string)=>{
 //例如 2019-12-11 10:11:00 显示为10:11:00
 Vue.filter('time', (value:string)=>{
   return value.split(' ')[1];
+});
+
+//例如 1000000.12  显示为 1 000 000.12
+Vue.filter('price', (num:any)=>{
+  num = num + "";
+  num = num.replace(/[ ]/g, ""); //去除空格
+  if (num == "") {
+    return;
+  }
+  if (isNaN(num)){
+    return;
+  }
+  //2.针对是否有小数点，分情况处理
+  let index = num.indexOf(".");
+  if (index==-1) {//无小数点
+    let reg = /(-?\d+)(\d{3})/;
+    while (reg.test(num)) {
+      num = num.replace(reg, "$1,$2");
+    }
+  } else {
+    let intPart = num.substring(0, index);
+    let pointPart = num.substring(index + 1, num.length);
+    let reg = /(-?\d+)(\d{3})/;
+    while (reg.test(intPart)) {
+      intPart = intPart.replace(reg, "$1,$2");
+    }
+    num = intPart +"."+ pointPart;
+  }
+  if(num.indexOf('.')<0){
+    return num+ '.00';
+  }else if(num.indexOf('.')===num.length-2){
+    return num+'0';
+  }else{
+    return num;
+  }
+});
+
+//例如 2019-12-11 10:11:00 显示为一小时后
+Vue.filter('after', (value:any)=>{
+  if(moment(value).diff(moment(),'days')>1){
+    return moment(value).diff(moment(),'days') + '天' + moment(value).diff(moment(),'hours')%24 + '小时';
+  }else if(moment(value).diff(moment(),'hours')>1){
+    return moment(value).diff(moment(),'hours') + '小时' + moment(value).diff(moment(),'minutes')%60 + '分钟';
+  }else if(moment(value).diff(moment(),'minutes')>1){
+    return moment(value).diff(moment(),'minutes') + '分钟' + moment(value).diff(moment(),'seconds')%60 + '秒';
+  }else if(moment(value).diff(moment(),'seconds')>1){
+    return moment(value).diff(moment(),'seconds') + '秒';
+  }else{
+    return '-';
+  }
 });
